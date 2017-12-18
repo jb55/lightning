d2ab44199bc62a0f067e39e8408a684df06f9cf2
William Casarin <jb55@jb55.com>
2017-12-18 02:15:05 -0800

    test: add tests for parse_ip_port
    This tests the basic functionality of parse_ip_port, which is used in
    parse_wireaddr.
    
    Signed-off-by: William Casarin <jb55@jb55.com>

---
+++ lightningd/test/run-ip_port_parsing.c
@@ -0,0 +1,33 @@
+
+#include <ccan/tal/str/str.h>
+#include <common/wireaddr.h>
+#include <common/utils.h>
+#include <stdbool.h>
+#include <assert.h>
+
+int main(void)
+{
+  tal_t *ctx = tal_tmpctx(NULL);
+  char *ip;
+  u16 port;
+
+  /* ret = getaddrinfo("[::1]:80", NULL, NULL, &res); */
+  assert(parse_ip_port(ctx, "[::1]:80", &ip, &port));
+  assert(streq(ip, "::1"));
+  assert(port == 80);
+
+  assert(!parse_ip_port(ctx, "::1", &ip, &port));
+  assert(streq(ip, "::1"));
+  assert(port == 0);
+
+  assert(parse_ip_port(ctx, "192.168.1.1:8000", &ip, &port));
+  assert(streq(ip, "192.168.1.1"));
+  assert(port == 8000);
+
+  assert(!parse_ip_port(ctx, "192.168.2.255", &ip, &port));
+  assert(streq(ip, "192.168.2.255"));
+  assert(port == 0);
+
+  tal_free(ctx);
+  return 0;
+}
